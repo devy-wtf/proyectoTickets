6c678dc8606e0dd6d850a6943a6245ad0437dcb9
devy-wtf <dasolisg.2005fwd@gmail.com>
2025-08-27 15:44:08 -0600

    funcionalidadCommit

--- scripts/pages/login.js
+++ scripts/pages/login.js
@@ -1,3 +1,50 @@
 import { loginUser, registerUser } from "../modules/app.js";
 
 const formulario = document.getElementById('loginForm')
+const loginBtn = document.getElementById('loginBtn')
+const regisBtn = document.getElementById('regisBtn')
+const aviso = document.getElementById('aviso')
+
+
+function getFormDatos() {
+    const formDatos = new FormData(formulario)
+    return{
+        estudianteID: formDatos.get('estudianteID').trim(),
+        password: formDatos.get('password').trim()  
+    }
+}
+
+loginBtn.addEventListener("click", async() => {
+    aviso.textContent = ''
+    const {estudianteID, password} =getFormDatos()
+
+    try{
+        const user = await loginUser(estudianteID, password)
+        if(user){
+            aviso.textContent= '¡Bienvenido, ${estudianteID}!'
+        }else{
+            aviso.textContent = "Los datos son incorrectos."
+        }
+        
+    } catch(Error){
+        aviso.textContent = `Error: ${Error.message}`
+    }
+    
+})
+
+regisBtn.addEventListener("click", async() => {
+    aviso.textContent = ''
+    const{estudianteID, password} = getFormDatos()
+
+
+    try{
+        const newUser = await registerUser (estudianteID, password)
+        if(newUser){
+            aviso.textContent = '¡Usuario ${newUser.estudianteID} registrado! Ya puedes iniciar sesión.'
+        }
+        } catch(Error){
+            aviso.textContent = 'Error: ${Error.message}'
+        }
+    }
+) 
+ 
